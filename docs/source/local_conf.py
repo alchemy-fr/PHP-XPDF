a07a27af033d8e8eb2743d128e248e862ab5e6fc
grosroro <grosroro@neutron>
2012-05-28 15:19:40 +0200

    Update layout

---
+++ docs/source/local_conf.py
@@ -0,0 +1,37 @@
+
+# project name
+project = u'PHP XPDF'
+
+#github repository URL
+github_repo = 'https://github.com/alchemy-fr/PHP-XPDF'
+
+# Tex info conf
+texinfo_documents = [
+  ('index', 'PHPXPDF', u'PHP XPDF Documentation',
+   u'Romain Neutron - Alchemy', 'PHPXPDF', 'One line description of project.',
+   'Miscellaneous'),
+]
+
+# Man page conf
+man_pages = [
+    ('index', 'phpxpdf', u'PHP XPDF Documentation',
+     [u'Romain Neutron - Alchemy'], 1)
+]
+
+# Latex doc conf
+latex_documents = [
+  ('index', 'PHPXPDF.tex', u'PHP XPDF Documentation',
+   u'Romain Neutron - Alchemy', 'manual'),
+]
+
+# html help basename
+htmlhelp_basename = 'PHPXPDFdoc'
+
+# version
+version = '0.1'
+
+# release
+release = '0.1'
+
+# Copyright Value  ex : copyright = u'2012, Alchemy'
+copyright = u'2012, Romain Neutron - Alchemy'
