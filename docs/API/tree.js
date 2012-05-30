bb9cf3e3bbc8634fea87c099b6a556fcfcd9a2c4
Romain Neutron <imprec@gmail.com>
2012-05-30 13:06:34 +0200

    Add API documentation

---
+++ docs/API/tree.js
@@ -0,0 +1,10 @@
+var tree = [["XPDF","XPDF.html","",
+                [["Exception","XPDF\/Exception.html","",
+                [["BinaryNotFoundException","XPDF\/Exception\/BinaryNotFoundException.html"," < Exception",
+                [        ]],["Exception","XPDF\/Exception\/Exception.html","",
+                [        ]],["InvalidFileArgumentException","XPDF\/Exception\/InvalidFileArgumentException.html"," < InvalidArgumentException",
+                [        ]],["LogicException","XPDF\/Exception\/LogicException.html"," < LogicException",
+                [        ]],["RuntimeException","XPDF\/Exception\/RuntimeException.html"," < RuntimeException",
+                [        ]]        ]],["PdfToText","XPDF\/PdfToText.html","",
+                [        ]]        ]]        ]
+
