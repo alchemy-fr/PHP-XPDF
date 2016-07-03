a7f7a4c694ebc5ac9178bb253de023cfdca2dec9
aikchun <aikchun616@gmail.com>
2016-07-03 21:46:32 +0800

    Changed parseOutputIntoData visibility to private and fixed formatting with php cs fixer

--- src/XPDF/PdfInfo.php
+++ src/XPDF/PdfInfo.php
@@ -32,98 +32,94 @@ use XPDF\Exception\BinaryNotFoundException;
  */
 class PdfInfo extends AbstractBinary
 {
-	private $data = array();
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getName()
-	{
-		return 'pdfinfo';
-	}
+    private $data = array();
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'pdfinfo';
+    }
 
-	public function getData() 
-	{
-		return $this->data;
-	}
+    public function getData()
+    {
+        return $this->data;
+    }
 
-	/**
-	 * Extracts the information of the current open PDF file, 
-	 *
-	 * @param string  $pathfile   The path to the PDF file
-	 *
-	 * @return array The extracted information as an associative array
-	 *
-	 * @throws InvalidArgumentException
-	 * @throws RuntimeException
-	 */
-	public function extractInfo($pathfile)
-	{
-		if ( ! file_exists($pathfile)) {
-			throw new InvalidArgumentException(sprintf('%s is not a valid file', $pathfile));
-		}
+    /**
+     * Extracts the information of the current open PDF file, 
+     *
+     * @param string  $pathfile   The path to the PDF file
+     *
+     * @return array The extracted information as an associative array
+     *
+     * @throws InvalidArgumentException
+     * @throws RuntimeException
+     */
+    public function extractInfo($pathfile)
+    {
+        if (! file_exists($pathfile)) {
+            throw new InvalidArgumentException(sprintf('%s is not a valid file', $pathfile));
+        }
 
-		$commands = array();
+        $commands = array();
 
-		$tmpFile = tempnam(sys_get_temp_dir(), 'pdfinfo');
+        $tmpFile = tempnam(sys_get_temp_dir(), 'pdfinfo');
 
-		$commands[] = $pathfile;
+        $commands[] = $pathfile;
 
-		try {
-			$output = $this->command($commands);
-			$this->data = $this->_parseOutputIntoData($output);
-			return $this->data;
-		} catch (ExecutionFailureException $e) {
-			throw new RuntimeException('Unale to extract text', $e->getCode(), $e);
-		}
+        try {
+            $output = $this->command($commands);
+            $this->data = $this->_parseOutputIntoData($output);
+            return $this->data;
+        } catch (ExecutionFailureException $e) {
+            throw new RuntimeException('Unale to extract text', $e->getCode(), $e);
+        }
+    }
 
-	}
+    /**
+     * Factory for PdfInfo
+     *
+     * @param array|Configuration $configuration
+     * @param LoggerInterface     $logger
+     *
+     * @return PdfInfo
+     */
+    public static function create($configuration = array(), LoggerInterface $logger = null)
+    {
+        if (!$configuration instanceof ConfigurationInterface) {
+            $configuration = new Configuration($configuration);
+        }
 
-	/**
-	 * Factory for PdfInfo
-	 *
-	 * @param array|Configuration $configuration
-	 * @param LoggerInterface     $logger
-	 *
-	 * @return PdfInfo
-	 */
-	public static function create($configuration = array(), LoggerInterface $logger = null)
-	{
-		if (!$configuration instanceof ConfigurationInterface) {
-			$configuration = new Configuration($configuration);
-		}
+        $binaries = $configuration->get('pdfinfo.binaries', 'pdfinfo');
 
-		$binaries = $configuration->get('pdfinfo.binaries', 'pdfinfo');
+        if (!$configuration->has('timeout')) {
+            $configuration->set('timeout', 60);
+        }
 
-		if (!$configuration->has('timeout')) {
-			$configuration->set('timeout', 60);
-		}
+        try {
+            return static::load($binaries, $logger, $configuration);
+        } catch (ExecutableNotFoundException $e) {
+            throw new BinaryNotFoundException('Unable to find pdfinfo', $e->getCode(), $e);
+        }
+    }
+    
 
-		try {
-			return static::load($binaries, $logger, $configuration);
-		} catch (ExecutableNotFoundException $e) {
-			throw new BinaryNotFoundException('Unable to find pdfinfo', $e->getCode(), $e);
-		}
-	}
-	
-
-	/**
-	 * Parse output string from pdfinfo command into associative array	 
-	 *
-	 * @param String $output 
-	 *
-	 * @return array
-	 */
-	protected function _parseOutputIntoData($output) 
-	{
-		$data = array();
-		$exploded = preg_split("/\\r\\n|\\r|\\n/", $output, -1, PREG_SPLIT_NO_EMPTY);
-		foreach ($exploded as $token) {
-			$keyValue = preg_split("/:/", $token, 2);
-			$data[$keyValue[0]] = trim($keyValue[1]);
-		}
-		return $data;
-	}
+    /**
+     * Parse output string from pdfinfo command into associative array	 
+     *
+     * @param String $output 
+     *
+     * @return array
+     */
+    private function parseOutputIntoData($output)
+    {
+        $data = array();
+        $exploded = preg_split("/\\r\\n|\\r|\\n/", $output, -1, PREG_SPLIT_NO_EMPTY);
+        foreach ($exploded as $token) {
+            $keyValue = preg_split("/:/", $token, 2);
+            $data[$keyValue[0]] = trim($keyValue[1]);
+        }
+        return $data;
+    }
 }
-
-
-
