78439a4ebef137e5f8db7f6734ce8921f33137e2
aikchun <aikchun616@gmail.com>
2016-07-03 21:49:10 +0800

    Reverted Licence

--- src/XPDF/PdfInfo.php
+++ src/XPDF/PdfInfo.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of PHP-XPDF.
  *
- * (c) AikChun <aikchun616@gmail.com>
+ * (c) Alchemy <info@alchemy.fr>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
