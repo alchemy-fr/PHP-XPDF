7c6eb424e1e80cd5dcffd5f813b27e8788f54b43
sandeep <sk@esokia-webagency.com>
2018-04-13 16:21:34 +0400

    fix sami

--- docs/sami_configuration.php
+++ docs/sami_configuration.php
@@ -6,16 +6,24 @@ use Sami\Sami;
 use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
+$dir = 'src';
+
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->in($dir = 'src')
+    ->in($dir)
+;
+
+$versions = GitVersionCollection::create($dir)
+    ->add('SILEX2', 'SILEX2 branch')
+    ->add('master', 'master branch')
 ;
 
 return new Sami($iterator, array(
     'title'                => 'PHP-XPDF API',
+    'versions'             => $versions,
     //'theme'                => 'enhanced',
-    'build_dir'            => __DIR__.'/source/API/API',
-    'cache_dir'            => __DIR__.'/source/API/API/cache',
+    'build_dir'            => __DIR__.'/docs/source/API/API/%version%',
+    'cache_dir'            => __DIR__.'/docs/source/API/API/%version%/cache',
     'default_opened_level' => 2,
 ));
